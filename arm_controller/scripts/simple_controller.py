33f758d3934a713adad444d4dc981cef8bad5502
Anand B <anand.bheemaraju@gmail.com>
2012-03-29 04:51:59 -0400

    Added code to control robot arm based on joint states

--- arm_controller/scripts/simple_controller.py
+++ arm_controller/scripts/simple_controller.py
@@ -15,9 +15,16 @@ from trajectory_msgs.msg import JointTrajectoryPoint
 from actionlib_msgs.msg._GoalStatus import GoalStatus
 from ee_cart_imped_action import EECartImpedClient
 from tabletop_object_detector.srv import TabletopDetection, TabletopDetectionRequest
-from arm_navigation_msgs.msg import MoveArmAction, MoveArmActionGoal, MoveArmGoal, JointConstraint
+from arm_navigation_msgs.msg import MoveArmResult, MoveArmAction, MoveArmActionGoal, MoveArmGoal, JointConstraint, PositionConstraint, SimplePoseConstraint, OrientationConstraint, Shape
 from kinematics_msgs.srv import GetPositionIK, GetPositionIKRequest, GetPositionIKResponse, GetKinematicSolverInfo, GetKinematicSolverInfoRequest
+import tf
 
+from getch import getch
+
+import re
+import time
+
+tf_listener = None
 
 def get_trajectory_point(positions, time_from_start):
     point = JointTrajectoryPoint()
@@ -27,7 +34,8 @@ def get_trajectory_point(positions, time_from_start):
     return point
 
 def task_completed(status, result):
-    print "Succeeded" if status == GoalStatus.SUCCEEDED else "Failed"
+    #print "Succeeded" if status == GoalStatus.SUCCEEDED else "Failed"
+    pass
 
 def move_body_part(service, joints, positions):
     client = actionlib.SimpleActionClient(service, JointTrajectoryAction)
@@ -40,9 +48,9 @@ def move_body_part(service, joints, positions):
     
     goal.trajectory.header.stamp = rospy.Time.now()
     client.send_goal(goal, done_cb=task_completed)
-    print "Goal Sent"
+    #print "Goal Sent"
     client.wait_for_result()
-    print "Success."
+    #print "Success."
 
 
 def move_right_arm(positions):
@@ -50,7 +58,7 @@ def move_right_arm(positions):
     
     joints = [ 'r_shoulder_pan_joint', # min(right_for_robot) = -2.1, max(left) = 0.56
                'r_shoulder_lift_joint', # min(up) = -0.35, max(down) = 1.28
-               'r_upper_arm_roll_joint', # min(clockwise_for_robot) = -3.75, max(anticlockwise) = 0.65
+               'r_upper_arm_roll_joint', # min(anti_clockwise_for_robot) = -3.75, max(clockwise) = 0.65
                'r_elbow_flex_joint', # min(near/up) = -2.12, max(away/down) = -0.15
                'r_forearm_roll_joint', # rotation joints need different logic, 5 = hand camera is down
                'r_wrist_flex_joint', # min(folded) = -2, max(open) = -0.1
@@ -85,11 +93,11 @@ def move_left_arm_initial_pose():
     move_left_arm(positions)
 
 def move_head_initial_pose():
-    positions = [-0.001, 0.714]
+    positions = [-0.001, 0.814]
     move_head(positions)
 
 def move_right_arm_pickup_start_state():
-    positions = [-0.5, -0.18, -2, -1.2, 5, -1.1, 0]
+    positions = [-0.5, -0.18, -2, -1.2, 5, -1.6, 0]
     move_right_arm(positions)
 
 def imped_right_arm_control():
@@ -118,37 +126,27 @@ def detect_objects():
         resp = table_object_detector(req)
         if resp.detection.result == resp.detection.SUCCESS:
             point = resp.detection.clusters[0].points[0]
-            print point
+            return (point.x, point.y, point.z)
         else:
             print resp.detection.result
     except rospy.ServiceException, e:
         print "Service call failed: %s" % e
 
 def move_arm_completed_cb(status, result):
-    print "Succeeded" if status == GoalStatus.SUCCEEDED else "Failed"
-    print type(result)
-    print result
+    if status == GoalStatus.SUCCEEDED:
+        print "Succeeded"  
+    else:
+        print "Failed"
+        print result.error_code.val
 
-def move_arm_ik():
-    service = "move_right_arm"
-    client = actionlib.SimpleActionClient(service, MoveArmAction)
-    client.wait_for_server()
-    
-    goal = MoveArmGoal()
-    goal.planner_service_name = "ompl_planning/plan_kinematic_path"
-    goal.motion_plan_request.group_name = "right_arm"
-    goal.motion_plan_request.num_planning_attempts = 1
-    goal.motion_plan_request.allowed_planning_time = roslib.rostime.Duration(5)
-    goal.motion_plan_request.planner_id = ""
 
-    joint_names = [  "r_shoulder_pan_joint",
-                     "r_shoulder_lift_joint",
-                     "r_upper_arm_roll_joint",
-                     "r_elbow_flex_joint",
-                     "r_forearm_roll_joint",
-                     "r_wrist_flex_joint",
-                     "r_wrist_roll_joint"]
-    
+def getJointConstraints(goal):
+    joint_names = ["r_shoulder_pan_joint", "r_shoulder_lift_joint",
+        "r_upper_arm_roll_joint",
+        "r_elbow_flex_joint",
+        "r_forearm_roll_joint",
+        "r_wrist_flex_joint",
+        "r_wrist_roll_joint"]
     joint_constraints = []
     for joint_name in joint_names:
         constraint = JointConstraint()
@@ -156,13 +154,82 @@ def move_arm_ik():
         constraint.position = 0
         constraint.tolerance_above = 0.1
         constraint.tolerance_below = 0.1
+        constraint.weight = 1.0
         joint_constraints.append(constraint)
     
     joint_constraints[0].position = -2
     joint_constraints[3].position = -0.2
     joint_constraints[5].position = -0.15
-    
     goal.motion_plan_request.goal_constraints.joint_constraints = joint_constraints
+
+
+def addPositionConstraint(goal, x, y, z):
+    constraint = PositionConstraint()
+    constraint.header.stamp = rospy.Time.now()
+    constraint.link_name = "r_wrist_roll_link"
+    constraint.header.frame_id = "base_link"
+    constraint.position.x = x
+    constraint.position.y = y
+    constraint.position.z = z
+    constraint.constraint_region_shape.type = Shape.BOX
+    constraint.constraint_region_shape.dimensions.append(0.01)
+    constraint.constraint_region_shape.dimensions.append(0.01)
+    constraint.constraint_region_shape.dimensions.append(0.01)
+    constraint.constraint_region_orientation.w = 1.0
+    constraint.weight = 1.0
+    goal.motion_plan_request.goal_constraints.position_constraints.append(constraint)
+
+
+def addOrientationConstraint(goal):
+    constraint = OrientationConstraint()
+    constraint.header.stamp = rospy.Time.now()
+    constraint.link_name = "r_wrist_roll_link"
+    constraint.header.frame_id = "base_link"
+    constraint.orientation.x = 0.0
+    constraint.orientation.y = 0.0
+    constraint.orientation.z = 0.0
+    constraint.orientation.w = 1.0
+    constraint.absolute_pitch_tolerance = 0.04
+    constraint.absolute_roll_tolerance = 0.04
+    constraint.absolute_yaw_tolerance = 0.04
+    constraint.weight = 1.0
+    goal.motion_plan_request.goal_constraints.orientation_constraints.append(constraint)
+
+def move_arm_ompl(x, y, z):
+    service = "move_right_arm"
+    client = actionlib.SimpleActionClient(service, MoveArmAction)
+    client.wait_for_server()
+    
+    goal = MoveArmGoal()
+    goal.disable_ik = True
+    goal.planner_service_name = "ompl_planning/plan_kinematic_path"
+    goal.motion_plan_request.group_name = "right_arm"
+    goal.motion_plan_request.num_planning_attempts = 5
+    goal.motion_plan_request.allowed_planning_time = roslib.rostime.Duration(5)
+    goal.motion_plan_request.planner_id = ""
+
+    addPositionConstraint(goal, x, y, z)
+    addOrientationConstraint(goal)
+    
+    '''
+    constraint = JointConstraint()
+    constraint.joint_name = "r_upper_arm_roll_joint"
+    constraint.position = -2
+    constraint.tolerance_above = 0.1
+    constraint.tolerance_below = 0.1
+    constraint.weight = 1.0
+    goal.motion_plan_request.goal_constraints.joint_constraints.append(constraint)
+    
+    '''
+    constraint = JointConstraint()
+    constraint.joint_name = "r_shoulder_lift_joint"
+    constraint.position = 0.4
+    constraint.tolerance_above = 0.2
+    constraint.tolerance_below = 0.2
+    constraint.weight = 1.0
+    goal.motion_plan_request.goal_constraints.joint_constraints.append(constraint)
+    
+    
     client.send_goal(goal, done_cb=move_arm_completed_cb)
     client.wait_for_result()
     
@@ -178,7 +245,10 @@ def get_ik_info():
     except rospy.ServiceException, e:
         print "Service call failed: %s" % e
 
-def solve_ik():
+def solve_ik(x, y, z):
+    '''
+    Need to call move_arm_ompl before calling this, otherwise it doesn't work. 
+    '''
     joint_names = [  "r_shoulder_pan_joint",
                      "r_shoulder_lift_joint",
                      "r_upper_arm_roll_joint",
@@ -194,18 +264,18 @@ def solve_ik():
         req = GetPositionIKRequest()
         req.timeout = roslib.rostime.Duration(5)
         req.ik_request.ik_link_name = "r_wrist_roll_link"
-        req.ik_request.pose_stamped.header.frame_id = "torso_lift_link"
+        req.ik_request.pose_stamped.header.frame_id = "base_link"
 
-        req.ik_request.pose_stamped.pose.position.x = 0.75;
-        req.ik_request.pose_stamped.pose.position.y = -0.188;
-        req.ik_request.pose_stamped.pose.position.z = 0.0;
+        req.ik_request.pose_stamped.pose.position.x = x
+        req.ik_request.pose_stamped.pose.position.y = y
+        req.ik_request.pose_stamped.pose.position.z = z
 
-        req.ik_request.pose_stamped.pose.orientation.x = 0.0;
-        req.ik_request.pose_stamped.pose.orientation.y = 0.0;
-        req.ik_request.pose_stamped.pose.orientation.z = 0.0;
-        req.ik_request.pose_stamped.pose.orientation.w = 1.0;
+        req.ik_request.pose_stamped.pose.orientation.x = 0.0
+        req.ik_request.pose_stamped.pose.orientation.y = 0.0
+        req.ik_request.pose_stamped.pose.orientation.z = 0.0
+        req.ik_request.pose_stamped.pose.orientation.w = 0.0
         
-        req.ik_request.ik_seed_state.joint_state.position = [0] * 7
+        req.ik_request.ik_seed_state.joint_state.position = [-0.5, -0.18, -2, -1.2, 5, -1.6, 0]
         req.ik_request.ik_seed_state.joint_state.name = joint_names
         
         resp = GetPositionIKResponse()
@@ -214,18 +284,153 @@ def solve_ik():
         print resp.solution.joint_state.name
         print resp.solution.joint_state.position
         
-        move_right_arm(resp.solution.joint_state.position)
+        if len(resp.solution.joint_state.position) == 7:
+            move_right_arm(resp.solution.joint_state.position)
+        
     except rospy.ServiceException, e:
         print "Service call failed: %s" % e
 
+def get_transform(link1, link2):
+    global tf_listener
+    if tf_listener is None:
+        tf_listener = tf.TransformListener()
+    while not rospy.is_shutdown():
+        try:
+            (trans, rot) = tf_listener.lookupTransform(link1, link2, rospy.Time(0))
+            return trans
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+
+
+def get_right_wrist_position():
+    return get_transform('/base_link', '/r_wrist_roll_link')
+    
+def get_dir(x, gx):
+    return 0 if (gx - x) == 0 else (gx - x) / abs(gx - x)
+
+def control_arm_joints():
+    #move_right_arm_pickup_start_state()
+    
+    positions = ['-0.100', '-0.354', '-2.000', '-1.900', '-1.283', '-1.100', '6.283']
+    positions = [float(x) for x in positions]
+    move_right_arm(positions)
+    step = 0.1
+    
+    stop = False
+    print "Enter Controls:"
+    while not stop:
+        c = getch()
+        c = c.lower()
+        if c not in ["a", "z", "s", "x", "d", "c", "f", "v", "g", "b", "h", "n", "j", "m"]:
+            print "Enter correct choice, q for exit"
+            continue
+        elif c == "a":
+            positions[0] += step
+        elif c == "z":
+            positions[0] -= step
+        elif c == "s":
+            positions[1] += step
+        elif c == "x":
+            positions[1] -= step
+        elif c == "d":
+            positions[2] += step
+        elif c == "c":
+            positions[2] -= step
+        elif c == "f":
+            positions[3] += step
+        elif c == "v":
+            positions[3] -= step
+        elif c == "g":
+            positions[4] += step
+        elif c == "b":
+            positions[4] -= step
+        elif c == "h":
+            positions[5] += step
+        elif c == "n":
+            positions[5] -= step
+        elif c == "j":
+            positions[6] += step
+        elif c == "m":
+            positions[6] -= step
+        elif c == "q":
+            stop = True
+            continue
+        move_right_arm(positions)
+
+def pick_food():
+    joint_names = [  "r_shoulder_pan_joint",
+                     "r_shoulder_lift_joint",
+                     "r_upper_arm_roll_joint",
+                     "r_elbow_flex_joint",
+                     "r_forearm_roll_joint",
+                     "r_wrist_flex_joint",
+                     "r_wrist_roll_joint"]
+    
+    input_string = """
+['-0.200', '-0.354', '-2.250', '-1.901', '-1.383', '-1.900', '6.283'] # pickup start state.
+['-0.080', '-0.18', '-2.250', '-1.901', '-1.383', '-1.900', '6.283'] #controls the downward push  
+['-0.080', '-0.18', '-2.250', '-1.901', '-1.383', '-0.75', '6.283'] # pickup food.
+['-0.200', '-0.354', '-1.500', '-1.901', '-1.383', '-1.600', '6.283'] # raise hand
+['-0.2', '-0.053', '-1.501', '-0.119', '-1.587', '-1.601', '6.285'] # move hand away
+"""
+    
+    positions_list = [[float(entry[1:-1]) for entry in input_line[1:].split("]")[0].split(", ")] for input_line in input_string.split("\n") if len(input_line) > 0]
+    #positions_list.insert(0, [-0.5, -0.18, -2, -1.2, 5, -1.6, 0])
+    for i, positions in enumerate(positions_list):
+        move_right_arm(positions)
+        a = input( "Done" + str(i+1) + "enter to continue:")
+        
+    
 if __name__ == '__main__':
     rospy.init_node('move_arm_trajectory_client')
+    pick_food()
+    #control_arm_joints()
+
+    #move_right_arm_initial_pose()
     '''
     move_right_arm_initial_pose()
     move_left_arm_initial_pose()
     move_head_initial_pose()
+    time.sleep(10)
+    detect_objects()
     move_right_arm_pickup_start_state()
+    time.sleep(10)
+    move_arm_ompl()
+    get_ik_info()
+    time.sleep(10)
     '''
-    solve_ik()
+    #move_right_arm_initial_pose()
     
+    #get_ik_info()
+    #move_right_arm_pickup_start_state()
+
+    """
+    #time.sleep(10)
     
+    '''
+    dx = get_dir(x, gx)
+    dy = get_dir(y, gy)
+    dz = get_dir(z, gz)
+    print dx, dy, dz
+    
+    step = 0.02
+    nx, ny, nz = x, y + step, z
+    print nx, ny, nz
+    '''
+    x, y, z = get_right_wrist_position()
+    print "wrist", x, y, z
+    
+    gx, gy, gz = detect_objects()
+    print "objects", gx, gy, gz
+    
+    nx, ny, nz = gx, gy, gz
+    move_arm_ompl(x, y - 0.02m, z)
+        
+    
+    #time.sleep(10)
+    #solve_ik(0.655698537827, 0.0111701283604, 0.523343801498)
+    #move_right_arm_pickup_start_state()
+    #time.sleep(10)
+    #x, y, z = detect_objects()
+    #print x, y, z
+    """
